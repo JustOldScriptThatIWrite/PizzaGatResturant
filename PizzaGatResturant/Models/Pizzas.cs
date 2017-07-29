1db1c682dcf4a17acd2be5e96dafa12c89f9ee63
big0ren <orenmaoz@gmail.com>
2017-07-29 21:56:56 +0300

    Before Computer Restart

---
+++ PizzaGatResturant/Models/Pizzas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Web;
+
+namespace PizzaGatResturant.Models
+{
+    public class Pizzas
+    {
+        public  int id { get; set; }
+        public string Name { get; set; }
+        
+    }
+    // Pizzas/Random
+}
